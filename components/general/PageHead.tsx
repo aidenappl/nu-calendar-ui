ed8bf5f323363dfbe8be1d0991f9e9dbb7fc55fd
Aiden Appleby <aiden.appleby3@gmail.com>
2023-01-19 11:03:00 -0500

    page head

---
+++ components/general/PageHead.tsx
@@ -0,0 +1,26 @@
+import { Head } from "next/document"
+
+
+
+const PageHead = () => {
+    return (
+        <Head>
+
+            <title>Northeastern CalFeed</title>
+            <meta name="title" content="Northeastern CalFeed">
+            <meta name="description" content="Modify your live-updating northeastern calendar to include location information, custom summaries and custom fields for complete control & customizability.">
+
+            <meta property="og:type" content="website">
+            <meta property="og:url" content="https://metatags.io/">
+            <meta property="og:title" content="Northeastern CalFeed">
+            <meta property="og:description" content="Modify your live-updating northeastern calendar to include location information, custom summaries and custom fields for complete control & customizability.">
+            <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
+
+            <meta property="twitter:card" content="summary_large_image">
+            <meta property="twitter:url" content="https://metatags.io/">
+            <meta property="twitter:title" content="Northeastern CalFeed">
+            <meta property="twitter:description" content="Modify your live-updating northeastern calendar to include location information, custom summaries and custom fields for complete control & customizability.">
+            <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"></meta>
+        </Head>
+    )
+}
