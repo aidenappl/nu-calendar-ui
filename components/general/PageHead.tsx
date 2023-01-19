81b6810eb744f3bb00a531edcd43117ceb38ddb4
Aiden Appleby <aiden.appleby3@gmail.com>
2023-01-19 11:14:23 -0500

    Added favicon and meta tags

--- components/general/PageHead.tsx
+++ components/general/PageHead.tsx
@@ -1,26 +1,74 @@
-import { Head } from "next/document"
-
-
+import Head from "next/head";
 
 const PageHead = () => {
-    return (
-        <Head>
+	return (
+		<Head>
+			<title>Northeastern CalFeed</title>
+			<meta name="title" content="Northeastern CalFeed" />
+			<meta
+				name="viewport"
+				content="initial-scale=1.0, width=device-width"
+			/>
 
-            <title>Northeastern CalFeed</title>
-            <meta name="title" content="Northeastern CalFeed">
-            <meta name="description" content="Modify your live-updating northeastern calendar to include location information, custom summaries and custom fields for complete control & customizability.">
+			{/* Favicon Header Items */}
+			<link
+				rel="apple-touch-icon"
+				sizes="180x180"
+				href="./favicons/apple-touch-icon.png"
+			/>
+			<link
+				rel="icon"
+				type="image/png"
+				sizes="32x32"
+				href="./favicons/favicon-32x32.png"
+			/>
+			<link
+				rel="icon"
+				type="image/png"
+				sizes="16x16"
+				href="./favicons/favicon-16x16.png"
+			/>
+			<link rel="manifest" href="/favicons/site.webmanifest" />
+			<link
+				rel="mask-icon"
+				href="./favicons/safari-pinned-tab.svg"
+				color="#5bbad5"
+			/>
+			<link rel="shortcut icon" href="./favicons/favicon.ico" />
+			<meta name="msapplication-TileColor" content="#70d2d1" />
+			<meta
+				name="msapplication-config"
+				content="./favicons/browserconfig.xml"
+			/>
+			<meta name="theme-color" content="#ffffff" />
 
-            <meta property="og:type" content="website">
-            <meta property="og:url" content="https://metatags.io/">
-            <meta property="og:title" content="Northeastern CalFeed">
-            <meta property="og:description" content="Modify your live-updating northeastern calendar to include location information, custom summaries and custom fields for complete control & customizability.">
-            <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
+			<meta
+				name="description"
+				content="Modify your live-updating northeastern calendar to include location information, custom summaries and custom fields for complete control & customizability."
+			/>
 
-            <meta property="twitter:card" content="summary_large_image">
-            <meta property="twitter:url" content="https://metatags.io/">
-            <meta property="twitter:title" content="Northeastern CalFeed">
-            <meta property="twitter:description" content="Modify your live-updating northeastern calendar to include location information, custom summaries and custom fields for complete control & customizability.">
-            <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"></meta>
-        </Head>
-    )
-}
+			<meta property="og:type" content="website" />
+			<meta property="og:url" content="https://nucal.aplb.xyz/" />
+			<meta property="og:title" content="Northeastern CalFeed" />
+			<meta
+				property="og:description"
+				content="Modify your live-updating northeastern calendar to include location information, custom summaries and custom fields for complete control & customizability."
+			/>
+			<meta property="og:image" content="./images/banner_image.jpg" />
+
+			<meta property="twitter:card" content="summary_large_image" />
+			<meta property="twitter:url" content="https://nucal.aplb.xyz/" />
+			<meta property="twitter:title" content="Northeastern CalFeed" />
+			<meta
+				property="twitter:description"
+				content="Modify your live-updating northeastern calendar to include location information, custom summaries and custom fields for complete control & customizability."
+			/>
+			<meta
+				property="twitter:image"
+				content="./images/banner_image.jpg"
+			/>
+		</Head>
+	);
+};
+
+export default PageHead;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import PageHead from "@/components/general/PageHead";
+import "@/styles/globals.css";
 
-export default function App({ Component, pageProps }: any) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }: any) => {
+	return (
+		<>
+			<PageHead />
+			<Component {...pageProps} />
+		</>
+	);
+};
+
+export default App;
